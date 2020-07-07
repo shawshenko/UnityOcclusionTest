57abd5b734a91fe5a31f3f7265db0d6e8fe5f56f
shawshenko <shawshenko@storymode.pro>
2020-07-07 17:01:03 +0300

    Cleaned up, removed unused code, added description

--- Assets/Scripts/LibUsage.cs
+++ Assets/Scripts/LibUsage.cs
@@ -12,7 +12,8 @@ public class LibUsage : MonoBehaviour
     [SerializeField] private LayerMask occlusionMask;
     public Text StatusText;
     public bool precise = false;
-    public int FOVResolution =1;
+    [Min(1)]public int FOVResolution =1;
+    private Collider targetCollider;
 
     public void DrawFOV(bool precise)
     {
@@ -23,25 +24,18 @@ public class LibUsage : MonoBehaviour
         if (precise)
             foreach (Vector3 point in CalculateRayPoints(FOVResolution))
             {
-                if (!OcclusionLib.CheckIfVisiblePrecise(point, refPoint.transform, target.transform, FOVAngle, occlusionMask))
+                if (!OcclusionLib.CheckIfVisiblePrecise(point, refPoint.transform, targetCollider, FOVAngle, occlusionMask))
                     Debug.DrawLine(refPoint.transform.position, point, Color.red);
                 else
                     Debug.DrawLine(refPoint.transform.position, point, Color.green);
             }
         else
         {
-            if (OcclusionLib.CheckIfVisible(target.transform, refPoint.transform, FOVAngle, occlusionMask))
+            if (OcclusionLib.CheckIfVisible(targetCollider, refPoint.transform, FOVAngle, occlusionMask))
                 Debug.DrawLine(refPoint.transform.position, target.transform.position, Color.green);
             else
                 Debug.DrawLine(refPoint.transform.position, target.transform.position, Color.red);
         }
-    }
-
-    
-    public void DrawTargetOutline()
-    {
-        Debug.DrawLine(refPoint.transform.position, target.GetComponent<Collider>().bounds.min, Color.red);
-        Debug.DrawLine(refPoint.transform.position, target.GetComponent<Collider>().bounds.max, Color.red);
     }
 
     public Vector3 DirFromAngle(float angleInDegrees, bool angleIsGlobal)
@@ -52,35 +46,36 @@ public class LibUsage : MonoBehaviour
         }
         return new Vector3(Mathf.Sin(angleInDegrees * Mathf.Deg2Rad), 0, Mathf.Cos(angleInDegrees * Mathf.Deg2Rad));
     }
-
+    
     public Vector3[] CalculateRayPoints(int resolution)
     {
         Vector3[] result = new Vector3[resolution];
-        Vector3 test = target.GetComponent<Collider>().bounds.max - target.GetComponent<Collider>().bounds.min;
+        Vector3 test = targetCollider.bounds.max - targetCollider.bounds.min;
         test = Vector3.Normalize(test);
-        float distance = Vector3.Distance(target.GetComponent<Collider>().bounds.max, target.GetComponent<Collider>().bounds.min);
+        float distance = Vector3.Distance(targetCollider.bounds.max, targetCollider.bounds.min);
         float modifier = distance / resolution;
         for(int i=0; i<resolution; i++)
         {
             if(i%2==0)
-                result[i] = target.GetComponent<Collider>().bounds.center + (modifier/2 * test * i);
+                result[i] = targetCollider.bounds.center + (modifier/2 * test * i);
             else
-                result[i] = target.GetComponent<Collider>().bounds.center - (modifier/2 * test * i);
+                result[i] = targetCollider.bounds.center - (modifier/2 * test * i);
         }
         return result;
 
     }
 
-
+    private void Start()
+    {
+        targetCollider = target.GetComponent<Collider>();
+    }
     void Update()
     {
         DrawFOV(precise);
-        
-
         StatusText.text = "Distance: " + OcclusionLib.GetDistance(target.transform, refPoint.transform) + "\n";
-        StatusText.text +="CheckIfVisible: "+ OcclusionLib.CheckIfVisible(target.transform, refPoint.transform, FOVAngle, occlusionMask).ToString()+"\n";
-        StatusText.text +="CheckIfInFOV: "+ OcclusionLib.CheckIfInFOV(target.transform, refPoint.transform, FOVAngle).ToString()+"\n";
-        StatusText.text +="CheckIfOccluded: "+ OcclusionLib.CheckIfOccluded(target.GetComponent<Collider>(), refPoint.transform, occlusionMask).ToString()+"\n";
+        StatusText.text +="CheckIfVisible: "+ OcclusionLib.CheckIfVisible(targetCollider, refPoint.transform, FOVAngle, occlusionMask).ToString()+"\n";
+        StatusText.text +="CheckIfInFOV: "+ OcclusionLib.CheckIfInFOV(target.transform.position, refPoint.transform, FOVAngle).ToString()+"\n";
+        StatusText.text +="CheckIfOccluded: "+ OcclusionLib.CheckIfOccluded(targetCollider, refPoint.transform, occlusionMask).ToString()+"\n";
     }
 }
 
---
+++ Assets/Scripts/OcclusionLib/OcclusionLib.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OcclusionLib
+{
+    public static bool CheckIfInFOV(Vector3 targetPosition, Transform referencePoint, float FOV)
+    {
+        Vector3 dirToTarget = (targetPosition - referencePoint.position).normalized;
+        if (Vector3.Angle(referencePoint.forward, dirToTarget) < FOV / 2)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public static bool CheckIfVisible(Collider target, Transform referencePoint, float FOV, LayerMask occlusionMask)
+    {
+        if(CheckIfInFOV(target.transform.position, referencePoint, FOV))
+            return !CheckIfOccluded(target.gameObject.GetComponent<Collider>(), referencePoint, occlusionMask);
+        return false;
+    }
+
+    public static bool CheckIfOccluded(Collider target, Transform referencePoint, LayerMask occlusionMask)
+    {
+        RaycastHit hitCollider;
+        var dir = target.transform.position - referencePoint.position;
+        bool hit = Physics.Raycast(referencePoint.position, dir, out hitCollider, dir.magnitude, occlusionMask);
+        return !(hit && hitCollider.collider == target);
+    }
+    
+    public static bool CheckIfVisiblePrecise(Vector3 targetPoint, Transform referencePoint, Collider target, float FOV, LayerMask occlusionMask)
+    {
+        if (CheckIfInFOV(targetPoint, referencePoint, FOV))
+            return !CheckIfOccludedPrecise(targetPoint, referencePoint, target, occlusionMask);
+        return false;
+    }
+
+    public static bool CheckIfOccludedPrecise(Vector3 targetPoint, Transform referencePoint, Collider target, LayerMask occlusionMask)
+    {
+        RaycastHit hitCollider;
+        var dir = targetPoint - referencePoint.position;
+        bool hit = Physics.Raycast(referencePoint.position, dir, out hitCollider, dir.magnitude, occlusionMask);
+        return !(hit && hitCollider.collider == target);
+    }
+
+    public static float GetDistance(Transform point1, Transform point2) {
+        return Vector3.Distance(point1.position, point2.position);
+    }
+
+   
+}
